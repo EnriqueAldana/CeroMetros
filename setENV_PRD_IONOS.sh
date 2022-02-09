e55acb954bad4072c57a133c40dfad0cfb41e1a3
enrique <jealdana@gmail.com>
2022-02-08 21:31:40 -0600

    Dump de MongoDb zmeters

---
+++ setENV_PRD_IONOS.sh
@@ -0,0 +1,15 @@
+export MAIL_URL="smtp://cerometros@jealdana.com:cerometros2022$@smtp.ionos.mx:26"
+export SENDER_EMAILS="{
+      "CONTACT": "cerometros@jealdana.com",
+      "SUPPORT": "cerometros@jealdana.com",
+      "SALES": "cerometros@jealdana.com"
+    }"
+export SENDER_EMAIL="cerometros@jealdana.com"
+export PRODUCT_IMAGE_PATH="https://firebasestorage.googleapis.com/v0/b/cerometros.appspot.com/o/ZeroMts_Product.png?alt=media&token=084767d8-190f-4fb6-8cab-ee1a40f2c00d"
+export LOGO_IMAGE_PATH="https://firebasestorage.googleapis.com/v0/b/cerometros.appspot.com/o/Logo.png?alt=media&token=0a923e0e-df2f-4201-a910-0d5a7ad735f2'"
+
+export REFRESH_PERMISSIONS="false"  
+export REFRESH_STATIC_PROFILES="false"
+export SHOW_ZERO_METERS_VERSION="true"
+export ZERO_METERS_USER="Cero Metros"
+export ZERO_METERS_VERSION="0.1"

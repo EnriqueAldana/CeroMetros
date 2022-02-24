c74b9d2f08482e1445bd9b5f2ffe24289d7fbcd7
enrique <jealdana@gmail.com>
2022-02-23 19:03:22 -0600

    Ajuste Logo

--- imports/ui/layouts/LytAuth.vue
+++ imports/ui/layouts/LytAuth.vue
@@ -1,41 +1,58 @@
 <template>
-    <v-row>
-        <v-col xs="12" sm="8" md="4" class="d-flex flex-column justify-space-around pa-12">
-            <div class="text-center">
-            
-                <img src="/img/LogoCeroMts.png" alt="Logo Cero Mts" id="Logo0Mts" height="180px" />
-              
-                <!-- img src="/img/vuetify.png" alt="Vuetify" height="180px" width="auto" / -->
-            </div>
-            <router-view name="sectionView"> </router-view>
-            <div class="text-center">
-                <img src="/img/Powered.png" alt="Powered by IngeCompu" id="poweredLogo" height="55px" />
-            </div>
-        </v-col>
-        <v-col xs="12" sm="4" md="8" class="right-side d-flex flex-column justify-center">
-              <div class="display-3 font-weight-medium mr-10 text-left white--text">
-                 
-              </div>
-        </v-col>
-    </v-row>
+  <v-row>
+    <v-col
+      xs="12"
+      sm="8"
+      md="4"
+      class="d-flex flex-column justify-space-around pa-12"
+    >
+      <div class="text-center">
+        <img
+          src="/img/LogoCeroMts.png"
+          alt="Logo Cero Mts"
+          id="Logo0Mts"
+          height="180px"
+        />
+
+        <!-- img src="/img/vuetify.png" alt="Vuetify" height="180px" width="auto" / -->
+      </div>
+      <router-view name="sectionView"> </router-view>
+      <div class="text-center">
+        <img
+          src="/img/Powered.png"
+          alt="Powered by IngeCompu"
+          id="poweredLogo"
+          height="55px"
+        />
+      </div>
+    </v-col>
+    <v-col
+      xs="12"
+      sm="4"
+      md="8"
+      class="right-side d-flex flex-column justify-center"
+    >
+     <!-- Aqui va la imagen del producto mediante el Style. -->
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-    export default {
-        name: "LytAuth"
-    }
+export default {
+  name: "LytAuth",
+};
 </script>
 
 <style scoped>
-    .display-3{
-        z-index: 2;
-        margin-top: 350px;
-    }
-
-    .right-side{
-        background-image: url("/img/ZeroMts_Product.png");
-        background-size: cover;
-        height: 100vh;
-        position: relative;
-    }
+.display-3 {
+  z-index: 2;
+  margin-top: 350px;
+}
+.right-side {
+  background-image: url("/img/ZeroMts_Product.png");
+  background-size: cover;
+  height: 100vh;
+  width:100vh;
+  position: relative;
+}
 </style>
--- setENV_PRD_IONOS.sh
+++ setENV_PRD_IONOS.sh
@@ -1,4 +1,4 @@
-export MAIL_URL="smtp://cerometros@jealdana.com:cerometros2022$@smtp.ionos.mx:26"
+#!/bin/bash
 export SENDER_EMAILS="{
       "CONTACT": "cerometros@jealdana.com",
       "SUPPORT": "cerometros@jealdana.com",
@@ -7,9 +7,12 @@ export SENDER_EMAILS="{
 export SENDER_EMAIL="cerometros@jealdana.com"
 export PRODUCT_IMAGE_PATH="https://firebasestorage.googleapis.com/v0/b/cerometros.appspot.com/o/ZeroMts_Product.png?alt=media&token=084767d8-190f-4fb6-8cab-ee1a40f2c00d"
 export LOGO_IMAGE_PATH="https://firebasestorage.googleapis.com/v0/b/cerometros.appspot.com/o/Logo.png?alt=media&token=0a923e0e-df2f-4201-a910-0d5a7ad735f2'"
-
+export ROOT_URL="http://jealdana.com"
 export REFRESH_PERMISSIONS="false"  
 export REFRESH_STATIC_PROFILES="false"
 export SHOW_ZERO_METERS_VERSION="true"
 export ZERO_METERS_USER="Sealed Air"
 export ZERO_METERS_VERSION="0.0.1"
+export PORT=80
+export TZ=America/Mexico_City
+export MONGO_URL=mongodb://root:CeroM3tros@jealdana.com:27017/zmeters?authSource=admin
